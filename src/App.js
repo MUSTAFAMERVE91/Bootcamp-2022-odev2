4c5c67fae7dd2a0b6afa8604f2e885329f08f401
mustafa-m <moustafa.bkor.1991@gmail.com>
2022-07-01 21:39:57 +0300

    loading files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Assistants from "./compenents/Assistants/Assistants";
 import "./App.css"
 import Students from "./compenents/Students/Students";
+import Persongroup from "./compenents/Persongroup/Persongroup";
 const App = () => {
 
   return (
     <div className="app">
       <Assistants></Assistants>
       <Students></Students>
+      <Persongroup></Persongroup>
       </div>
 
   );
